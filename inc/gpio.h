9c603cbd024214e1e4e25c2af2eb1f7f440617c9
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-08-31 09:38:04 -0300

    Add GPIO helper

---
+++ inc/gpio.h
@@ -0,0 +1,10 @@
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#define FAN_MIN_VALUE 40
+
+void setup_gpio();
+void update_resistor();
+void update_fan();
+
+#endif
---
+++ src/gpio.c
@@ -0,0 +1,22 @@
+#include <gpio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+#include <gpio.h>
+#include <definitions.h>
+
+void setup_gpio() {
+  wiringPiSetup();
+  softPwmCreate(GPIO_RESISTOR, 0, GPIO_PWM_RANGE);
+  softPwmCreate(GPIO_FAN, FAN_MIN_VALUE, GPIO_PWM_RANGE);
+}
+
+void update_resistor(int value) {
+  softPwmWrite(GPIO_RESISTOR, value);
+}
+
+void update_fan(int value) {
+  if (value < FAN_MIN_VALUE)
+    value = FAN_MIN_VALUE;
+  softPwmWrite(GPIO_FAN, value);
+}
