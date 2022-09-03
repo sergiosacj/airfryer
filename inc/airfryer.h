3534cca8f6303429d3cca5933997983f9179ba58
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-09-03 07:54:38 -0300

    Add state_machine and control_internal_temperature

--- inc/airfryer.h
+++ inc/airfryer.h
@@ -2,6 +2,8 @@
 #define AIRFRYER_H_
 
 void start_airfryer();
+void state_machine();
+void control_internal_temperature();
 void stop_airfryer();
 
 #endif
--- src/airfryer.c
+++ src/airfryer.c
@@ -1,28 +1,72 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <signal.h>
 
 #include <definitions.h>
 #include <modbus.h>
 #include <lcd.h>
 #include <pid.h>
 
+static float reference_temperature = 0;
+static double internal_temperature = 0;
+static double control_signal = 0;
+static int milisecond_counter = 0;
+static int timer = 0;
+
 void start_airfryer() {
   printf("Aguardando comando para ligar a airfryer...\n");
+  pid_setup_constants(KP, KI, KD);
+  signal(SIGINT, handle_sigint);
+  // SETUP INITIAL VALUTE TO ZERO
 
   while (1) {
-    int value_i = get_user_commands();
-    if (value_i == USER_CMD_TURN_ON)
+    int user_command = get_user_commands();
+    if (user_command == USER_CMD_TURN_ON)
       break;
-    else if (value_i != 0)
+    else if (user_command != 0)
       printf("Nenhum comando será processado com a airfryer desligada!\n");
   }
 
   send_system_state(1);
 }
 
+void state_machine() {
+  switch (milisecond_counter) {
+    default:
+      break;
+  }
+  usleep(100000);
+}
+
+void control_internal_temperature() {
+  reference_temperature = get_reference_temperature();
+  pid_update_reference(reference_temperature);
+
+  internal_temperature = get_internal_temperature();
+  control_signal = pid_control(internal_temperature);
+
+  if (control_signal < 100) {
+    update_resistor(0);
+    update_fan(-1 * control_signal);
+  } else {
+    update_fan(0);
+    update_resistor(control_signal);
+  }
+
+  send_control_signal(control_signal);
+}
+
 void stop_airfryer() {
-  // desligar ventoinha e resistor
+  int ret = 1;
+  while (ret != 0)
+    ret = raise(SIGINT);
+}
+
+void handle_sigint(int signum) {
   send_working_state(0);
   send_system_state(0);
+  int ret = 1;
+  while (ret != 0)
+    ret = raise(SIGUSR1);
 }
