cd5b2931b8694ce5fccafc15b7709b5db599a9af
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-09-03 14:02:24 -0300

    Manage temperature with lcd variables

--- src/airfryer.c
+++ src/airfryer.c
@@ -19,12 +19,10 @@ static void start_heating();
 static void stop_heating();
 static void change_menu_option();
 static void update_timer(int value);
-static void control_internal_temperature(int reference_temperature);
+static void control_internal_temperature();
 static void create_csv();
 static void update_csv();
 
-static double reference_temperature;
-static double internal_temperature;
 static double control_signal;
 static int milisecond_counter;
 static int menu_option;
@@ -32,6 +30,12 @@ static bool heating;
 DisplayLCD lcd;
 
 void start_airfryer() {
+  lcd = start_display();
+  if (lcd.fd == -1) {
+    printf("Falha ao iniciar display LCD!");
+    stop_airfryer();
+  }
+
   default_values();
 
   printf("Criando arquivo de log: %s\n", CSV_FILE_NAME);
@@ -56,12 +60,6 @@ void start_airfryer() {
   }
 
   send_system_state(1);
-
-  lcd = start_display();
-  if (lcd.fd == -1) {
-    printf("Falha ao iniciar display LCD!");
-    stop_airfryer();
-  }
 
   while (1) {
     milisecond_counter = 0;
@@ -116,8 +114,8 @@ static void state_machine() {
     milisecond_counter++;
     milisecond_counter%=10;
     if (milisecond_counter == 5 || milisecond_counter == 0) {
-      reference_temperature = get_reference_temperature();
-      control_internal_temperature(reference_temperature);
+      lcd.reference_temperature = get_reference_temperature();
+      control_internal_temperature(lcd.reference_temperature);
       process_user_commands();
     }
     if (milisecond_counter == 0) {
@@ -134,9 +132,9 @@ static void state_machine() {
 static void start_heating() {
   heating = true;
   int count = 0;
-  while (reference_temperature > internal_temperature) {
-    reference_temperature = get_reference_temperature();
-    control_internal_temperature(reference_temperature);
+  while (lcd.reference_temperature > lcd.internal_temperature) {
+    lcd.reference_temperature = get_reference_temperature();
+    control_internal_temperature(lcd.reference_temperature);
     usleep(200000);
     if (count == 0)
       draw_heating_cooling(&lcd, 'H');
@@ -147,9 +145,9 @@ static void start_heating() {
 static void stop_heating() {
   heating = false;
   int count = 0;
-  reference_temperature = 25; // environment temperature
-  while (reference_temperature < internal_temperature) {
-    control_internal_temperature(reference_temperature);
+  lcd.reference_temperature = 25; // environment temperature
+  while (lcd.reference_temperature < lcd.internal_temperature) {
+    control_internal_temperature(lcd.reference_temperature);
     usleep(200000);
     if (count == 0)
       draw_heating_cooling(&lcd, 'C');
@@ -168,12 +166,12 @@ static void change_menu_option() {
   switch (menu_option) {
     case 1:
       lcd.menu_option = 1;
-      reference_temperature = 50;
+      lcd.reference_temperature = 50;
       lcd.timer = 60 * 3;
       break;
     case 2:
       lcd.menu_option = 2;
-      reference_temperature = 40;
+      lcd.reference_temperature = 40;
       lcd.timer = 60 * 2;
       break;
     default:
@@ -193,11 +191,11 @@ static void update_timer(int value) {
   send_timer(lcd.timer);
 }
 
-static void control_internal_temperature(int reference_temperature) {
-  pid_update_reference(reference_temperature);
+static void control_internal_temperature() {
+  pid_update_reference(lcd.reference_temperature);
 
-  internal_temperature = get_internal_temperature();
-  control_signal = pid_control(internal_temperature);
+  lcd.internal_temperature = get_internal_temperature();
+  control_signal = pid_control(lcd.internal_temperature);
 
   if (control_signal < 100) {
     update_resistor(0);
@@ -231,11 +229,11 @@ static void handle_sigint(int signum) {
 
 static void default_values() {
   printf("Reiniciando valores iniciais.\n");
-  reference_temperature = 0;
-  internal_temperature = 0;
+  lcd.reference_temperature = 0;
+  lcd.internal_temperature = 0;
   control_signal = 0;
   milisecond_counter = 0;
-  timer = TIME_MIN;
+  lcd.timer = TIME_MIN;
   menu_option = 0;
   heating = false;
 }
@@ -255,7 +253,7 @@ static void update_csv() {
   timeinfo = localtime(&rawtime);
   fprintf(file, "%02d,%02d,%02d", timeinfo->tm_mday, timeinfo->tm_mon, timeinfo->tm_year + 1900);
   fprintf(file, ",%02d,%02d,%02d", timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-  fprintf(file, ",%lf,%lf,%lf", internal_temperature, reference_temperature, control_signal);
+  fprintf(file, ",%lf,%lf,%lf", lcd.internal_temperature, lcd.reference_temperature, control_signal);
 
   fclose(file);
 }
